53b813da206649d886ff92fa107f20a73f04241c
Andre Oliveira <andre.dasilva@bairesdev.com>
2024-11-05 15:52:52 -0300

    fix: repository_lifecycle_policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,25 +21,23 @@ resource "aws_ecr_repository" "carpentry_tools" {
   name                 = "carpentry-tools"
   image_tag_mutability = "MUTABLE" # Set repository to allow mutable tags
 
-  lifecycle_policy {
-    policy = jsonencode({
-      rules = [
-        {
-          rulePriority = 1,
-          description  = "Keep last 2 images",
-          selection = {
-            tagStatus     = "tagged",
-            tagPrefixList = ["v"],
-            countType     = "imageCountMoreThan",
-            countNumber   = 2
-          },
-          action = {
-            type = "expire"
-          }
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 2 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 2
+        },
+        action = {
+          type = "expire"
         }
-      ]
-    })
-  }
+      }
+    ]
+  })
 
   tags = {
     Terraform   = "true"
